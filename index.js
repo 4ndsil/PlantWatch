ddbe7158ffcc664203145e30a748d451e99054c4
Lucas Villarroel <vilucas7@gmail.com>
2022-12-03 13:27:28 +0100

    setup template engine

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
+const { engine } = require("express-handlebars");
 
 const app = express();
 const PORT = 3000;
 
+// handlebars engine, set partails directory
+app.engine(
+  "handlebars",
+  engine({
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
+
+// set view engine & directory
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
 app.listen(PORT, (error) => {
-    if (!error)
-        console.log("Server is Successfully Running, and App is listening on port " + PORT)
-    else
-        console.log("Error occurred, server can't start", error);
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
 });
