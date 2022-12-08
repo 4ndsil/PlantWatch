135394531cf26b7d07a87f65d2c0e33436473fd3
andsil <andreas.siljefors@gmail.com>
2022-12-08 14:16:26 +0100

    public route

---
+++ server/routes/setup.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/setup", async (req, res, next) => { 
+  const{plantId, deviceId} = req.body
+  res.cookie('plantId', `${plantId}`, { maxAge: 900000, httpOnly: true });
+  res.cookie('deviceId', `${deviceId}`, { maxAge: 900000, httpOnly: true });
+  res.render("dashboard");
+});
+
+module.exports = router;
